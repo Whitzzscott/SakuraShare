0c321ebc578b83e78441cf9bb288df6c7f56cdc7
whitzscott <whitzscott@gmail.com>
2025-01-29 19:06:16 +0800

    added roll up

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,19 @@
 import resolve from '@rollup/plugin-node-resolve';
+import babel from '@rollup/plugin-babel';
 
 export default {
-  plugins: [resolve()],
+  input: 'src/App.jsx', // Assuming the entry file is App.jsx
+  output: {
+    file: 'dist/bundle.js',
+    format: 'esm',
+  },
+  plugins: [
+    resolve({
+      extensions: ['.js', '.jsx'], // Make sure .jsx files are resolved
+    }),
+    babel({
+      babelHelpers: 'bundled',
+      extensions: ['.js', '.jsx'], // Support JSX files in Babel
+    }),
+  ],
 };
